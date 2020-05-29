7618e9962365889f890eb68fc23fa75e2560c774
LARIOSD <47404918+LARIOSD@users.noreply.github.com>
2020-05-29 17:02:31 -0500

    0_0

--- Controller/actions/act_register.php
+++ Controller/actions/act_register.php
@@ -6,27 +6,41 @@ require_once(__DIR__ . "/../../Model/entities/Usuario.php");
 $nombre = $_POST['nombre'];
 $username = $_POST['correo'];
 $password = $_POST['password'];
-$image = $_POST['image'];
 $tipo = $_POST['tipo'];
 $telefono = $_POST['telefono'];
 $direccion = $_POST['direccion'];
 
+if (isset($_FILES['Imagen'])) {
+    $imagen = addslashes(file_get_contents($_FILES['Imagen']['tmp_name']));
+}
 
-if (!empty($_POST['correo']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
-    if (($_POST['password'] == $_POST['confirm_password'])) {
+$usuarios = leerUsuarios();
+$estado = 0;
+foreach ($usuarios as $aux) :
+    if ($username === $aux['correo']) {
+        $estado = 1;
+    }
+endforeach;
 
-        $usuario = new Usuario(
-            null,
-            $nombre,
-            $username,
-            $password,
-            null,
-            $tipo,
-            $telefono,
-            $direccion,
-        );
+if ($estado === 1) {
+    header("Location: ../../view/admin/crud_usuario.php?id=0"); //NO SE PUDO REGISTRAR EL USUARIO PORQUE EL CORREO YA EXISTE
+} else {
+    if (!empty($_POST['correo']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
+        if (($_POST['password'] == $_POST['confirm_password'])) {
 
-        $respuesta = insertarUsuario($usuario);
+            $usuario = new Usuario(
+                null,
+                $nombre,
+                $username,
+                $password,
+                null,
+                $tipo,
+                $telefono,
+                $direccion,
+            );
+
+            $respuesta = insertarUsuario($usuario);
+        }
     }
 }
 
